14147c8c288ab0b384e624ffd906e0648458d486
BrieAnn Prater <bprater@my365.bellevue.edu>
2020-08-07 15:31:59 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Assignment9_Prater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.introLabel = new System.Windows.Forms.Label();
+            this.guessOne = new System.Windows.Forms.Button();
+            this.guessTwo = new System.Windows.Forms.Button();
+            this.nxtButton = new System.Windows.Forms.Button();
+            this.instructLabel = new System.Windows.Forms.Label();
+            this.correctPanel = new System.Windows.Forms.Panel();
+            this.correctCnt = new System.Windows.Forms.Label();
+            this.correct = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.ncorrectCnt = new System.Windows.Forms.Label();
+            this.ncorrect = new System.Windows.Forms.Label();
+            this.num1 = new System.Windows.Forms.Label();
+            this.num2 = new System.Windows.Forms.Label();
+            this.correctPanel.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // introLabel
+            // 
+            this.introLabel.AutoSize = true;
+            this.introLabel.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.introLabel.Location = new System.Drawing.Point(100, 40);
+            this.introLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.introLabel.Name = "introLabel";
+            this.introLabel.Size = new System.Drawing.Size(404, 22);
+            this.introLabel.TabIndex = 0;
+            this.introLabel.Text = "Which one will contain the higher number?";
+            // 
+            // guessOne
+            // 
+            this.guessOne.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessOne.Location = new System.Drawing.Point(175, 200);
+            this.guessOne.Margin = new System.Windows.Forms.Padding(4);
+            this.guessOne.Name = "guessOne";
+            this.guessOne.Size = new System.Drawing.Size(100, 30);
+            this.guessOne.TabIndex = 3;
+            this.guessOne.Text = "1";
+            this.guessOne.UseVisualStyleBackColor = true;
+            this.guessOne.Click += new System.EventHandler(this.guessOne_Click);
+            // 
+            // guessTwo
+            // 
+            this.guessTwo.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessTwo.Location = new System.Drawing.Point(325, 200);
+            this.guessTwo.Margin = new System.Windows.Forms.Padding(4);
+            this.guessTwo.Name = "guessTwo";
+            this.guessTwo.Size = new System.Drawing.Size(100, 30);
+            this.guessTwo.TabIndex = 4;
+            this.guessTwo.Text = "2";
+            this.guessTwo.UseVisualStyleBackColor = true;
+            this.guessTwo.Click += new System.EventHandler(this.guessTwo_Click);
+            // 
+            // nxtButton
+            // 
+            this.nxtButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nxtButton.Location = new System.Drawing.Point(436, 370);
+            this.nxtButton.Margin = new System.Windows.Forms.Padding(4);
+            this.nxtButton.Name = "nxtButton";
+            this.nxtButton.Size = new System.Drawing.Size(94, 30);
+            this.nxtButton.TabIndex = 5;
+            this.nxtButton.Text = "Next";
+            this.nxtButton.UseVisualStyleBackColor = true;
+            this.nxtButton.Click += new System.EventHandler(this.nxtButton_Click);
+            // 
+            // instructLabel
+            // 
+            this.instructLabel.AutoSize = true;
+            this.instructLabel.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructLabel.Location = new System.Drawing.Point(81, 70);
+            this.instructLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.instructLabel.Name = "instructLabel";
+            this.instructLabel.Size = new System.Drawing.Size(438, 22);
+            this.instructLabel.TabIndex = 6;
+            this.instructLabel.Text = "Make your choice, View your score, then click Next.";
+            // 
+            // correctPanel
+            // 
+            this.correctPanel.Controls.Add(this.correctCnt);
+            this.correctPanel.Controls.Add(this.correct);
+            this.correctPanel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.correctPanel.Location = new System.Drawing.Point(65, 260);
+            this.correctPanel.Name = "correctPanel";
+            this.correctPanel.Size = new System.Drawing.Size(200, 75);
+            this.correctPanel.TabIndex = 7;
+            // 
+            // correctCnt
+            // 
+            this.correctCnt.AutoSize = true;
+            this.correctCnt.Location = new System.Drawing.Point(75, 45);
+            this.correctCnt.Name = "correctCnt";
+            this.correctCnt.Size = new System.Drawing.Size(0, 18);
+            this.correctCnt.TabIndex = 1;
+            this.correctCnt.Click += new System.EventHandler(this.correctCnt_Click);
+            // 
+            // correct
+            // 
+            this.correct.AutoSize = true;
+            this.correct.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.correct.Location = new System.Drawing.Point(20, 4);
+            this.correct.Name = "correct";
+            this.correct.Size = new System.Drawing.Size(160, 22);
+            this.correct.TabIndex = 0;
+            this.correct.Text = "Correct Guesses:";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.ncorrectCnt);
+            this.panel2.Controls.Add(this.ncorrect);
+            this.panel2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel2.Location = new System.Drawing.Point(330, 260);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 75);
+            this.panel2.TabIndex = 8;
+            // 
+            // ncorrectCnt
+            // 
+            this.ncorrectCnt.AutoSize = true;
+            this.ncorrectCnt.Location = new System.Drawing.Point(75, 45);
+            this.ncorrectCnt.Name = "ncorrectCnt";
+            this.ncorrectCnt.Size = new System.Drawing.Size(0, 18);
+            this.ncorrectCnt.TabIndex = 1;
+            this.ncorrectCnt.Click += new System.EventHandler(this.ncorrectCnt_Click);
+            // 
+            // ncorrect
+            // 
+            this.ncorrect.AutoSize = true;
+            this.ncorrect.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ncorrect.Location = new System.Drawing.Point(14, 4);
+            this.ncorrect.Name = "ncorrect";
+            this.ncorrect.Size = new System.Drawing.Size(172, 22);
+            this.ncorrect.TabIndex = 0;
+            this.ncorrect.Text = "Incorrect Guesses:";
+            // 
+            // num1
+            // 
+            this.num1.AutoSize = true;
+            this.num1.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num1.Location = new System.Drawing.Point(200, 135);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(0, 56);
+            this.num1.TabIndex = 9;
+            this.num1.Click += new System.EventHandler(this.num1_Click_1);
+            // 
+            // num2
+            // 
+            this.num2.AutoSize = true;
+            this.num2.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num2.Location = new System.Drawing.Point(350, 135);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(0, 56);
+            this.num2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.correctPanel);
+            this.Controls.Add(this.instructLabel);
+            this.Controls.Add(this.nxtButton);
+            this.Controls.Add(this.guessTwo);
+            this.Controls.Add(this.guessOne);
+            this.Controls.Add(this.introLabel);
+            this.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Guessing Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.correctPanel.ResumeLayout(false);
+            this.correctPanel.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label introLabel;
+        private System.Windows.Forms.Button guessOne;
+        private System.Windows.Forms.Button guessTwo;
+        private System.Windows.Forms.Button nxtButton;
+        private System.Windows.Forms.Label instructLabel;
+        private System.Windows.Forms.Panel correctPanel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label correctCnt;
+        private System.Windows.Forms.Label correct;
+        private System.Windows.Forms.Label ncorrectCnt;
+        private System.Windows.Forms.Label ncorrect;
+        private System.Windows.Forms.Label num1;
+        private System.Windows.Forms.Label num2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿/* Brie Prater
+ * 07/31/2020
+ * CIS 353
+ * Professor Cain
+ * Assignment 9.2 */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment9_Prater
+{
+    public partial class Form1 : Form // instatiates the form
+    {
+        int[] rand1 = new int[100]; // instantiates array with 100 indexes
+        int[] rand2 = new int[100]; // instantiates array with 100 indexes
+        bool one; // used to determine if the guess was correct
+        int right = 0; // variable to count correct guesses
+        int wrong = 0; // variable to count incorrect guesses
+        int iteration = 0; // variable to count iterations and used as index for arrays 
+        Random rando = new Random(); // instatiates Random function
+ 
+        public Form1() // initializes form
+        {
+            InitializeComponent();
+        }
+
+        private void createArray() // method to populate arrays
+        {
+            for (int x = 0; x < 100; x++) // for loop to fill 100 indexes
+            {
+                rand1[x] = rando.Next(0, 100); // fills each index with a random number
+                rand2[x] = rando.Next(0, 100); 
+            }
+        }
+        private void compareNum(int[] rand1, int[] rand2, int iteration) // method to compare which number is higher
+            // passes arrays and iteration to method
+        {
+            if (rand1[iteration] > rand2[iteration]) // boolean to determine which array has a higher number at index of iteration
+            {
+                this.num1.Visible = true; // displays num1 label if it is higher
+                this.num1.Text = rand1[iteration].ToString(); // sets label text to the number at the index
+                this.num2.Visible = false; // hides num2 label
+                one = true; // set to true for num1 being higher
+                 
+            }
+            else if (rand2[iteration] > rand1[iteration]) // boolean to determine which array has a higher number at index of iteration
+            {
+                this.num2.Visible = true; // displays num2 label if it is higher
+                this.num2.Text = rand2[iteration].ToString(); // sets label text to the number at the index
+                this.num1.Visible = false; // hides num1 label
+                one = false; // set to false for num2 being higher
+
+            }
+            else
+            {
+                this.num1.Text = rand1[iteration].ToString(); // displays both labels if the numbers are equal
+                this.num2.Text = rand2[iteration].ToString();
+            }
+        }
+
+        private void clickedIt() // method to disable buttons
+        {
+            guessOne.Enabled = false; // disables button 1
+            guessTwo.Enabled = false; // disables button 2
+        }
+        private void guessOne_Click(object sender, EventArgs e) // method for when button 1 is clicked
+        {
+            compareNum(rand1, rand2, iteration); // calls compareNum method and passes rand1 and rand2 arrays, and the iteration num
+            clickedIt(); // calls clickedIt method
+            if (one) // boolean if one is set to true, num1 is higher
+            {
+                right++; // since button 1 was clicked, this is a correct guess and right counter is increased
+            }
+            else // num1 was not higher
+            { 
+                wrong++; // since button 1 was clicked, this is an incorrect guess and wrong counter is increased
+            }
+            this.correctCnt.Text = right.ToString(); // sets correctCnt label text to right variable
+            this.ncorrectCnt.Text = wrong.ToString(); // sets ncorrectCnt label text to wrong variable
+        }
+
+        private void guessTwo_Click(object sender, EventArgs e) // method for when button 2 is clicked
+        {
+            compareNum(rand1, rand2, iteration); // calls compareNum method and passes rand1 and rand2 arrays, and the iteration num
+            clickedIt(); // calls clickedIt method
+            if (one != true) // boolean if one is set to false, num2 is higher
+            {
+                right++; // since button 2 was clicked, this is a correct guess and right counter is increased
+            }
+            else // num2 was not higher
+            {
+                wrong++; // since button 2 was clicked, this is an incorrect guess and wrong counter is increased
+            }
+            this.correctCnt.Text = right.ToString(); // sets correctCnt label text to right variable
+            this.ncorrectCnt.Text = wrong.ToString(); // sets ncorrectCnt label text to wrong variable
+        }
+
+        private void correctCnt_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ncorrectCnt_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void nxtButton_Click(object sender, EventArgs e) // method for when Next button is clicked
+        {
+            if (iteration < 100) // checks that iteration is less than 100, which is total number of indexes in each array
+            {
+                this.num1.Visible = false; // hides num1 label text
+                this.num2.Visible = false; // hides num2 label text
+                guessOne.Enabled = true; // enables 1 button
+                guessTwo.Enabled = true; // enables 2 button
+                iteration++; // increases iteration count
+            }
+            else // 100 guesses have been made, arrays must be repopulated
+            {
+                createArray(); // calls createArray method
+                iteration = 0; // sets iteration back to zero
+            }
+
+        }
+
+        private void num1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void num2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e) // method for when Form1 loads
+        {
+            createArray(); // calls createArray to populate arrays at initialization
+        }
+
+        private void num1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
